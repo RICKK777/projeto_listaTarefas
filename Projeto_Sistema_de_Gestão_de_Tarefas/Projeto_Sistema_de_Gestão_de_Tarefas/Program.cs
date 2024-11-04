5671c5cb1df01908ad0a50a15f76333da15eb079
RICKK777 <henrique.maltoni26@gmail.com>
2024-11-04 14:04:42 -0300

    Add files via upload

---
+++ Projeto_Sistema_de_Gestão_de_Tarefas/Projeto_Sistema_de_Gestão_de_Tarefas/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Sistema_de_Gestão_de_Tarefas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prioridade = new int[5];
+            string[] tarefas = new string[5];
+    
+
+            while(true)
+            {
+                Console.WriteLine("Escolha a uma opção");
+                Console.WriteLine("A - adiconar tarefa");
+                Console.WriteLine("L - listar tarefa");
+                Console.WriteLine("E - excluir tarefa");
+                Console.WriteLine("F - fechar o programa");
+
+                var escolha = Console.ReadLine();
+                Console.Clear();
+                if (escolha.ToUpper() == "A")
+                {
+                   
+
+                    for (int i = 0; i < prioridade.Length; i++)
+                    {
+                        Console.WriteLine("Adicione sua Tarefa: ");
+                        tarefas[i] = Console.ReadLine();
+
+                        Console.WriteLine("Digite a prioridade da tarefa (1 a 5): ");
+                        prioridade[i] = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Tarefa adicionada!!");
+                        return;
+                    }
+                }
+                else if (escolha.ToUpper() == "L")
+                {
+
+                }
+                else if (escolha.ToUpper() == "E")
+                {
+
+                }
+                else if(escolha.ToUpper() == "F")
+                {
+                    break;
+                }
+            }
+
+            
+           
+
+        }
+    }
+}
